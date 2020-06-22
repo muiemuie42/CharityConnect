c34d8b90b560a417d03bd88bde1f8f207ece5847
Horatiu Mitrea <horatiumitrea@Horatius-MacBook-Air.local>
2020-06-22 18:56:25 -0400

    Configuring OAuth w/ Twitter

--- server/server.js
+++ server/server.js
@@ -1,20 +1,13 @@
 const express = require('express');
 const path =  require('path');
-const app = express()
+const app = express();
 const port = 3000;
 
-const leaderList = [
-    { name: "Anna", id: "a0" },
-    { name: "Ben", id: "b0" },
-    { name: "Clara", id: "c0" },
-    { name: "David", id: "d0" },
-  ];
+
+const passport = require('../server/passport-config/passport');
+  // console.log('This is our node env ', process.env.NODE_ENV);
   
-  app.get("/api/leaders", (req, res) => {
-    res.send(leaderList);
-  });
-  console.log('This is our node env ', process.env.NODE_ENV);
-  
+
 if (process.env.NODE_ENV === "production") {
     // statically serve everything in the build folder on the route '/build'
     app.use("/build", express.static(path.join(__dirname, "../build")));
@@ -24,4 +17,21 @@ if (process.env.NODE_ENV === "production") {
     });
   }
 
+  app.use(passport.initialize()); // initialize user session
+  app.use(passport.session()); // store user's info in a session
+
+
+
+  app.get('/login/twitter', passport.authenticate('twitter')); // send request to twitter
+  app.get('/login', (req, res)=>{
+    console.log('loging in without oauth')
+  })
+  app.get('/logout', (req, res)=>{
+    console.log('logout')
+  })
+
+  app.get('/twitter/callback', passport.authenticate('twitter', { failureRedirect: '/login' }), // get a response and direct to failure if it's a failure
+  function(req, res) { res.redirect('/');  }); // otherwise the homepage
+
+  
 app.listen(3000);
