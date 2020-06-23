aa3e471b87b3d87c5ef0f4088fc80d5723cef620
Horatiu Mitrea <horatiumitrea@Horatius-MacBook-Air.local>
2020-06-23 11:14:54 -0400

    Twitter Oauth WOrking

--- server/server.js
+++ server/server.js
@@ -22,22 +22,21 @@ if (process.env.NODE_ENV === "production") {
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieparser());
+app.use(require('express-session')({secret:'keyboard cat', resave: false, saveUninitialized: false, cookie: {secure:false}})) // We want it secure:false since we're NOT ussing HTTPS
 app.use(passport.initialize()); // initialize user session
 app.use(passport.session()); // store user's info in a session
 
+app.get('/login/twitter', passport.authenticate('twitter')); // send request to twitter
+app.get('/login', userController.createUser, (req, res)=>{
+  console.log('loging in without oauth', res.locals.user)
+  res.sendStatus(200)
+})
+app.get('/logout', (req, res)=>{
+  console.log('logout')
+})
 
-
-  app.get('/login/twitter', passport.authenticate('twitter')); // send request to twitter
-  app.get('/login', userController.createUser, (req, res)=>{
-    console.log('loging in without oauth', res.locals.user)
-    res.sendStatus(200)
-  })
-  app.get('/logout', (req, res)=>{
-    console.log('logout')
-  })
-
-  app.get('/twitter/callback', passport.authenticate('twitter', { failureRedirect: '/login' }), // get a response and direct to failure if it's a failure
-  function(req, res) { res.redirect('/');  }); // otherwise the homepage
-
+app.get('/twitter/callback', passport.authenticate('twitter', { failureRedirect: '/' }), // get a response and direct to failure if it's a failure
+  function(req, res) { res.redirect('http://localhost:8080/');  // otherwise the homepage
+}); 
   
 app.listen(3000);
